20ea69756cc11d960c9ae3665192c70f1df7e34e
AdiGunturu <75342181+adigunturu@users.noreply.github.com>
2021-07-17 06:07:52 +0530

    Create script.js

---
+++ public/js/script.js
@@ -0,0 +1,86 @@
+const socket = io.connect('/', { transports: ['websocket'] });
+const videoGrid = document.getElementById("video-grid");
+const myPeer = new Peer(undefined, {
+  secure:true,
+  host: "smart-talk-peerjs.herokuapp.com",
+  port: 443
+});
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+const peers = {};
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+
+    myPeer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      console.log("New User Connected"+ " " + userId);
+      setTimeout(connectToNewUser,1000,userId,stream)
+    });
+  });
+
+socket.on("user-disconnected", (userId) => {
+  console.log("User Disconnected"+ " " + userId);
+  if (peers[userId]) peers[userId].close();
+});
+
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);console.log("works")
+  });
+  call.on("close", () => {
+    video.remove();
+  });
+
+  peers[userId] = call;
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
+
+// URL Copy To Clipboard
+document.getElementById("invite-button").addEventListener("click", getURL);
+
+function getURL() {
+  const c_url = window.location.href;
+  copyToClipboard(c_url);
+ // alert("Url Copied to Clipboard,\nShare it with your Friends!\nUrl: " + c_url);
+}
+
+function copyToClipboard(text) {
+  var dummy = document.createElement("textarea");
+  document.body.appendChild(dummy);
+  dummy.value = text;
+  dummy.select();
+  document.execCommand("copy");
+  document.body.removeChild(dummy);
+}
+
+// End Call
+document.getElementById("end-button").addEventListener("click", endCall);
+
+function endCall() {
+  window.location.href = "/";
+}
